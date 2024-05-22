c9ec55185108be8e4acfbe2861b6738376aa182f
Yann Le Doaré <yann@linuxconsole.org>
2024-05-22 05:52:21 +0000

    update script

--- sql.sh
+++ sql.sh
@@ -1,7 +1,18 @@
-for count in 7 # 1 2 3 4 5 6 7
+DB_HOST=$(grep DB_HOST /var/www/framadate/app/inc/config.php | cut -d"'" -f2)
+DB_NAME=$(grep DB_NAME /var/www/framadate/app/inc/config.php | cut -d"'" -f2)
+DB_USER=$(grep DB_USER /var/www/framadate/app/inc/config.php | cut -d"'" -f2)
+DB_PASSWORD=$(grep DB_PASSWORD /var/www/framadate/app/inc/config.php | cut -d"'" -f2)
+
+CHOICE="14,16"
+
+for poll in poll-name
 do
-  DAY=$(($count * 86400))
-  echo $DAY
-  mysql -h mysql -u framadate -pframadatepasswd framadate -e "INSERT INTO fd_slot(id,poll_id,title,moments) SELECT MAX( id ) + 1, 'G5fe5WaWSB5wbkUj',  UNIX_TIMESTAMP() + $DAY + 86400, '14,16' FROM fd_slot"
-  mysql -h mysql -u framadate -pframadatepasswd framadate -e "INSERT INTO fd_slot(id,poll_id,title,moments) SELECT MAX( id ) + 1, '4tHX6pHNQG99ttZ2',  UNIX_TIMESTAMP() + $DAY + 86400, '14,16' FROM fd_slot"
+  NEXT=$((7 * 86400))
+  DAY=`date +'%u'`
+  [ "$DAY" = 2 ] && continue
+  [ "$DAY" = 7 ] && continue
+
+   mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME -e "select count(*) from fd_slot where title < (UNIX_TIMESTAMP() - 172800) and poll_id='$poll'" | grep 0 || mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME -e "update fd_vote set choices=substring(choices,2,100)"
+   mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME -e "delete from fd_slot where title <  (UNIX_TIMESTAMP() -  172800) and poll_id='$poll'"
+   mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME -e "INSERT INTO fd_slot(id,poll_id,title,moments) SELECT MAX( id ) + 1, '$poll',  UNIX_TIMESTAMP() + $NEXT, '$CHOICE' FROM fd_slot"
 done
